9d7cbed36e38ec8c92bea332db4373db0ce7a99b
cubeDHuang <99danielh@gmail.com>
2018-03-18 21:13:56 -0400

    better password generator

--- random/passwords/script.js
+++ random/passwords/script.js
@@ -1,32 +1,51 @@
 var mems = [
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
-	"thing other word new choose password wow look".split(" "),
+	["temp"],
 	"1234567890".split(""),
 	"!@$%&".split("")
 ];
+
+fetch("./words.txt").then(res => res.text()).then(text => {
+	mems[0] = text.split("\n");
+	setPasswords();
+});
+
 var easyChars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM".split("");
-var strongChars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-=[];\'\"\\<>/".split("");
-var insaneChars = "`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?".split("");
+var strongChars ="1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-=[];\'\"\\<>/".
+	split("");
+var insaneChars =
+"`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?".
+	split("");
+
+strongChars[strongChars.indexOf("<")] = "&lt;";
+strongChars[strongChars.indexOf("&")] = "&amp;";
+insaneChars[insaneChars.indexOf("<")] = "&lt;";
+insaneChars[insaneChars.indexOf("&")] = "&amp;";
 
 function randInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + (min));
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function pick(array) {
+	return array[randInt(0, array.length - 1)];
 }
 
 function grsp(array, length) {
 	var string = "";
-	for (var i = 0; i < length; i++) string += array[randInt(0, array.length - 1)];
+	for (var i = 0; i < length; i++) string += pick(array);
 	return string;
 }
 
 function setPasswords() {
-	var mem = "";
-	mem += mems[0][Math.floor(Math.random() * mems[0].length)];
-	mem += mems[1][Math.floor(Math.random() * mems[1].length)];
-	mem += mems[1][Math.floor(Math.random() * mems[1].length)];
-	mem += mems[2][Math.floor(Math.random() * mems[2].length)];
-	if (Math.random() > 0.5) mem += mems[2][Math.floor(Math.random() * mems[2].length)];
-	mem += mems[3][Math.floor(Math.random() * mems[3].length)];
-	document.getElementById("memorable").innerHTML = mem;
+	if (mems[1].length !== 1) {
+		var mem = "";
+		var w1 = pick(mems[0]);
+		mem += w1[0].toUpperCase() + w1.slice(1);
+		mem += pick(mems[1]);
+		mem += pick(mems[1]);
+		mem += pick(mems[2]);
+
+		document.getElementById("memorable").innerHTML = mem;
+	} else document.getElementById("memorable").innerHTML = "LOADING WORDS...";
 
 	document.getElementById("medium").innerHTML = grsp(easyChars, 8);
 	document.getElementById("strong").innerHTML = grsp(strongChars, 12);
