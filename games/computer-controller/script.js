040aec1b6a899a572d107075a16cc3159e1f1167
cubeDhuang <99danielh@gmail.com>
2018-07-15 10:54:20 -0400

    Many Things

--- games/computer-controller/script.js
+++ games/computer-controller/script.js
@@ -6,7 +6,14 @@ numberformat.default.opts = {
 new Vue({
 	el: "#game",
 
+	created() {
+		this.loop(0);
+		this.amounts = this.items.map(() => new Decimal(0));
+	},
+
 	data: {
+		pTime: 0,
+
 		bits: new Decimal(10000000),
 
 		items: [
@@ -39,8 +46,8 @@ new Vue({
 				desc: "An extremely powerful computer."
 			},
 			{
-				name: "Meteoroid computer",
-				desc: "A computer the size of a meteoroid."
+				name: "orbital computer",
+				desc: "A computer that orbits a star."
 			},
 			{
 				name: "Asteriod computer",
@@ -55,8 +62,8 @@ new Vue({
 				desc: "A computer encapsulating a star."
 			},
 			{
-				name: "Black Hole computer",
-				desc: "A computer harnessing a black hole."
+				name: "Dark computer",
+				desc: "A computer harnessing dark energy."
 			},
 			{
 				name: "Galactic computer",
@@ -66,12 +73,65 @@ new Vue({
 				name: "Time computer",
 				desc: "A computer that uses time travel."
 			},
-		]
+			{
+				name: "universal computer",
+				desc: "A computer that utilizes a universe."
+			},
+			{
+				name: "Universe",
+				desc: "Simulation time!"
+			},
+			{
+				name: "Multiverse",
+				desc: "All possible worlds with our physics."
+			},
+			{
+				name: "Omniverse",
+				desc: "All possible worlds with our dimensions."
+			},
+			{
+				name: "Everything",
+				desc: "All possible Omniverses."
+			},
+		],
+
+		amounts: []
+	},
+
+	computed: {
+		max() {
+			return this.items.map((_, i, a) => {
+				if (i === 0) return Decimal.floor(Decimal.pow(60, i).times(8).div(this.bits));
+				return Decimal.min(
+					Decimal.floor(Decimal.pow(60, i).times(8).div(this.bits)),
+					Decimal.floor(Decimal.pow(1.7, i).div(this.amounts[i - 1]))
+				);
+			});
+		},
+		able() {
+			return this.items.map((_, i, a) => {
+				return Decimal.min(
+					Decimal.floor(Decimal.pow(60, i).times(8).div(this.bits)),
+					Decimal.floor(Decimal.pow(1.7, i).div(i - 1))
+				);
+			});
+		}
 	},
 
 	methods: {
 		format(x) {
 			return numberformat.format(x);
+		},
+		capital(s) {
+			if (!s) return;
+			return s.split(" ").map(s => `${ s[0].toUpperCase() }${ s.slice(1) }`).join(" ");
+		},
+
+		loop(ms) {
+			requestAnimationFrame(this.loop);
+			const passed = (ms - this.pTime) / 1000;
+			
+			this.pTime = ms;
 		}
 	}
 });
