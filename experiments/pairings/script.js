b3a32e8b530bbfaaa81da0b10ad6669a902e40f3
cubedhuang <99danielh@gmail.com>
2020-07-31 20:54:40 -0400

    make genR funct inaccesible

--- experiments/pairings/script.js
+++ experiments/pairings/script.js
@@ -33,58 +33,58 @@ const debug = new Vue({
 			this.pairings = shuffled;
 		},
 		
-		generateR(type) {
-			function isAny(val, ...checks) {
-				let output = false;
-				for (let i = 0; i < checks.length; i++) {
-					if (val === checks[i]) output = true;
-				}
-				return output;
-			}
+		// generateR(type) {
+		// 	function isAny(val, ...checks) {
+		// 		let output = false;
+		// 		for (let i = 0; i < checks.length; i++) {
+		// 			if (val === checks[i]) output = true;
+		// 		}
+		// 		return output;
+		// 	}
 
-			function checkContain(arr, ...checks) {
-				let output = true;
-				for (let i = 0; i < checks.length; i++) {
-					if (!arr.includes(checks[i])) output = false;
-				}
-				return output;
-			}
+		// 	function checkContain(arr, ...checks) {
+		// 		let output = true;
+		// 		for (let i = 0; i < checks.length; i++) {
+		// 			if (!arr.includes(checks[i])) output = false;
+		// 		}
+		// 		return output;
+		// 	}
 
-			if (!checkContain(this.debaters, "Karthik", "Pranaya", "Ananya", "Saanvi", "Daniel") || type === 2) {
-				this.generate(type);
-				return;
-			}
+		// 	if (!checkContain(this.debaters, "Karthik", "Pranaya", "Ananya", "Saanvi", "Daniel") || type === 2) {
+		// 		this.generate(type);
+		// 		return;
+		// 	}
 
-			this.type = type;
+		// 	this.type = type;
 
-			let shuffled = this.debaters.map(c => c);
+		// 	let shuffled = this.debaters.map(c => c);
 
-			for (let i = shuffled.length - 1; i > 0; i--) {
-				let j = Math.floor(Math.random() * (i + 1));
-				[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-			}
+		// 	for (let i = shuffled.length - 1; i > 0; i--) {
+		// 		let j = Math.floor(Math.random() * (i + 1));
+		// 		[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+		// 	}
 
-			let setIndex = 0;
-			for (let i = 0; i < shuffled.length; i++) {
-				if (isAny(shuffled[i], "Karthik", "Pranaya")){
-					[shuffled[i], shuffled[setIndex]] = [shuffled[setIndex], shuffled[i]];
-					setIndex++;
-				}
-			}
-			for (let i = 0; i < shuffled.length; i++) {
-				if (isAny(shuffled[i], "Ananya", "Saanvi")){
-					[shuffled[i], shuffled[setIndex]] = [shuffled[setIndex], shuffled[i]];
-					setIndex++;
-				}
-			}
-			for (let i = 0; i < shuffled.length; i++) {
-				if (isAny(shuffled[i], "Daniel")){
-					[shuffled[i], shuffled[setIndex]] = [shuffled[setIndex], shuffled[i]];
-					setIndex++;
-				}
-			}
+		// 	let setIndex = 0;
+		// 	for (let i = 0; i < shuffled.length; i++) {
+		// 		if (isAny(shuffled[i], "Karthik", "Pranaya")){
+		// 			[shuffled[i], shuffled[setIndex]] = [shuffled[setIndex], shuffled[i]];
+		// 			setIndex++;
+		// 		}
+		// 	}
+		// 	for (let i = 0; i < shuffled.length; i++) {
+		// 		if (isAny(shuffled[i], "Ananya", "Saanvi")){
+		// 			[shuffled[i], shuffled[setIndex]] = [shuffled[setIndex], shuffled[i]];
+		// 			setIndex++;
+		// 		}
+		// 	}
+		// 	for (let i = 0; i < shuffled.length; i++) {
+		// 		if (isAny(shuffled[i], "Daniel")){
+		// 			[shuffled[i], shuffled[setIndex]] = [shuffled[setIndex], shuffled[i]];
+		// 			setIndex++;
+		// 		}
+		// 	}
 
-			this.pairings = shuffled;
-		}
+		// 	this.pairings = shuffled;
+		// }
 	}
 });
