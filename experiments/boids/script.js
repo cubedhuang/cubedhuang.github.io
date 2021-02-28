9ea8fd40d6a0da34a812020106ee6ee8de23446f
cubedhuang <99danielh@gmail.com>
2021-02-27 20:23:18 -0500

    desired, neighbors, noise, changed mouse & limits, physics fix

--- experiments/boids/script.js
+++ experiments/boids/script.js
@@ -1,10 +1,15 @@
 const flock = [];
 
 let debugP;
-let pauseC, directionC, hueC, vision1C, vision2C;
-let alignS, cohesionS, separationS, alignP, cohesionP, separationP;
+let pauseC, directionC, desiredC, hueC, vision1C, vision2C, neighborsC;
+let alignS, alignP,
+	cohesionS, cohesionP,
+	separationS, separationP;
+let trailS, trailP;
+let noiseS, noiseP;
 let visionS, visionP;
 let maxForceS, maxForceP, maxSpeedS, maxSpeedP;
+let mouseForce;
 
 function setup() {
 	createCanvas(800, 600);
@@ -14,39 +19,53 @@ function setup() {
 	
 	pauseC = createCheckbox(" paused", false);
 	directionC = createCheckbox(" show direction", true);
+	desiredC = createCheckbox(" show desired direction", false);
 	hueC = createCheckbox(" change hue to indicate speed", true);
 	vision1C = createCheckbox(" show vision areas", false);
 	vision2C = createCheckbox(" show vision outlines", false);
+	neighborsC = createCheckbox(" show visible neighbors", false);
 	bounceC = createCheckbox(" bounce off of edges", false);
 
-	visionP = createP("vision: 75");
-	visionS = createSlider(0, 250, 75, 5);
+	trailP = createP("trail opacity: 50");
+	trailS = createSlider(0, 120, 50, 5);
+
+	noiseP = createP("movement randomness: 0.2");
+	noiseS = createSlider(0, 5, 2, 0.5);
+
+	visionP = createP("boid vision: 50");
+	visionS = createSlider(0, 200, 50, 5);
 
 	alignP = createP("alignment force: 1");
-	alignS = createSlider(0, 5, 1, 0.1);
+	alignS = createSlider(0, 4, 1, 0.1);
 	cohesionP = createP("cohesion force: 1");
-	cohesionS = createSlider(0, 5, 1, 0.1);
+	cohesionS = createSlider(0, 4, 1, 0.1);
 	separationP = createP("separation force: 1");
-	separationS = createSlider(0, 5, 1, 0.1);
+	separationS = createSlider(0, 4, 1, 0.1);
 
 	maxForceP = createP("steering force: 0.2");
-	maxForceS = createSlider(0, 2, 0.2, 0.05);
+	maxForceS = createSlider(0, 1, 0.2, 0.05);
 
 	maxSpeedP = createP("max speed: 10");
 	maxSpeedS = createSlider(0, 20, 10, 0.5);
 
 	background(31);
 	for (let i = 0; i < 150; i++) {
-		flock.push(new Boid());
+		flock.push(new Boid(i));
 	}
+
+	document.querySelector("canvas").addEventListener("contextmenu", e => e.preventDefault());
 }
 
 function draw() {
 	if (!pauseC.checked()) {
-		background(31, 31, 31, 180);
+		background(31, 31, 31, 255 - trailS.value());
+
+		mouseForce = max((alignS.value() + cohesionS.value() + separationS.value()) / 3, 1);
 	
 		for (const boid of flock) {
 			boid.flock(flock);
+		}
+		for (const boid of flock) {
 			boid.update();
 			boid.showData();
 		}
@@ -54,7 +73,7 @@ function draw() {
 			boid.showSelf();
 		}
 	} else {
-		background(31, 31, 31, 180);
+		background(31, 31, 31, 255);
 	
 		for (const boid of flock) {
 			boid.showData();
@@ -64,14 +83,12 @@ function draw() {
 		}
 	}
 
-	visionP.html("vision: " + visionS.value());
+	trailP.html("trail opacity: " + trailS.value());
+	noiseP.html("movement randomness: " + noiseS.value());
+	visionP.html("boid vision: " + visionS.value());
 	alignP.html("alignment force: " + alignS.value());
 	cohesionP.html("cohesion force: " + cohesionS.value());
 	separationP.html("separation force: " + separationS.value());
 	maxForceP.html("steering force: " + maxForceS.value());
 	maxSpeedP.html("max speed: " + maxSpeedS.value());
 }
-
-setTimeout(() => {
-	document.querySelector("canvas").addEventListener('contextmenu', event => event.preventDefault());
-}, 1000);
