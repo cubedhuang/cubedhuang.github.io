6310ee5177a7b65da774e56886eb023ae69e97ea
cubedhuang <99danielh@gmail.com>
2021-04-30 17:32:01 -0400

    improve infinite buttons

--- games/infinite-buttons/script.js
+++ games/infinite-buttons/script.js
@@ -12,39 +12,34 @@ let debug = new Vue({
 	},
 
 	methods: {
-		create(i) {
+		create(i, event) {
+			// prevent enter
+			if (event.x === 0 && event.y === 0) return;
+
 			let temp = this.buttons.slice();
 			this.score += temp[i].value;
 
 			let bgc = ["red"  , "blue" , "green", "yellow"];
 
 			let i1 = Math.floor(Math.random() * bgc.length);
-			let i2 = Math.floor(Math.random() * bgc.length);
 
-			temp.push({
+			if (temp.length <= 50) temp.push({
 				value: Math.trunc(Math.random() * 100) + 1,
 				bg: bgc[i1],
 				x: temp[i].x,
 				y: temp[i].y,
 			});
-			if (temp.length <= 50) temp.push({
-				value: Math.trunc(Math.random() * 100) + 1,
-				bg: bgc[i2],
-				x: temp[i].x,
-				y: temp[i].y,
-			});
-
-			temp.splice(i, 1);
 
 			this.buttons = temp;
+			this.changePos();
 		},
 		
 		changePos() {
 			let temp = this.buttons.slice();
 
 			for (var i = 0; i < temp.length; i++) {
-				temp[i].x = Math.random() * (window.innerWidth - 300) + 100;
-				temp[i].y = Math.random() * (window.innerHeight - 100) + 50;
+				temp[i].x = Math.random() * (window.innerWidth - 80);
+				temp[i].y = Math.random() * (window.innerHeight - 80);
 			}
 
 			this.buttons = temp;
